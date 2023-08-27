fc096bb83d1c80aaa1d2669ad818ffe38fc2bfc6
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2023-08-27 20:36:47 +0630

    udpate database

--- admin/Model/dbConnection.php
+++ admin/Model/dbConnection.php
@@ -1,9 +1,9 @@
 <?php
-$hostname = "vrcarer-do-user-14558665-0.b.db.ondigitalocean.com";
+$hostname = "vr-carer-kzh-do-user-14558665-0.b.db.ondigitalocean.com";
 $port = "25060";
 $dbname = "vr_carer";
 $username = "doadmin";
-$password = "AVNS_qJNTCAbwJjf4AvW3bJ0";
+$password = "AVNS_GG5ThkhbtY0roQXhZHE";
 $pdo = new PDO(
     "mysql:host=$hostname;port=$port;dbname=$dbname", 
 $username, $password);
